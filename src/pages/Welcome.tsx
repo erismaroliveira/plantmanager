1e5a2eed34b936d367e99959a11e6a6b9181d070
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-08 20:14:50 -0300

    Component Welcome

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { 
+    SafeAreaView, 
+    Text, 
+    Image, 
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+
+export function Welcome() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas {'\n'}
+                de forma fácil
+            </Text>
+
+            <Image 
+                source={wateringImg} 
+                style={styles.image} 
+                resizeMode="contain"
+            />
+
+            <Text style={styles.subtitle}>
+                Não esqueça mais de regar suas plantas. 
+                Nós cuidamos de lembrar você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>
+                    <Entypo name="chevron-thin-right" />
+                </Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56,
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24,
+    }
+})
