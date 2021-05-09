28a658aef1ae0c2c1020d19de3fd7ce49f4ed053
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-08 21:54:34 -0300

    Update welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,42 +5,46 @@ import {
     Image, 
     TouchableOpacity,
     StyleSheet,
-    Dimensions
+    Dimensions,
+    View
 } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
-
-            <Image 
-                source={wateringImg} 
-                style={styles.image} 
-                resizeMode="contain"
-            />
-
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. 
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity 
-                style={styles.button} 
-                activeOpacity={0.7}
-            >
-                <Text style={styles.buttonText}>
-                    <Entypo name="chevron-thin-right" />
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
                 </Text>
-            </TouchableOpacity>
 
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image} 
+                    resizeMode="contain"
+                />
+
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. 
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                >
+                    <Feather 
+                        name="chevron-right"
+                        style={styles.buttonIcon}
+                    />  
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -48,21 +52,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 15
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     image: {
         height: Dimensions.get('window').width * 0.7
@@ -76,8 +87,8 @@ const styles = StyleSheet.create({
         height: 56,
         width: 56,
     },
-    buttonText: {
+    buttonIcon: {
+        fontSize: 32,
         color: colors.white,
-        fontSize: 24,
     }
 })
