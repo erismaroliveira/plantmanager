c23fe771b68dc8e2bbd93f98b7217f177ab03bf4
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-09 22:36:04 -0300

    View page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import {
   useFonts,
   Jost_400Regular,
@@ -17,6 +17,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
