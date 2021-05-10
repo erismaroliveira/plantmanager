e2e74029f6422a158aa8648033caea639c712849
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-09 23:47:01 -0300

    Update page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 import {
   useFonts,
   Jost_400Regular,
@@ -17,6 +17,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <UserIdentification />
+    <Confirmation />
   )
 }
