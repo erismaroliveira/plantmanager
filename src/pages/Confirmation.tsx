6cd539832a84ecc2d6e18e2a522f576ea9bd6efe
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-11 11:38:07 -0300

    Add navigation component

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -29,7 +29,9 @@ export function Confirmation() {
         </Text>
 
         <View style={styles.footer}>
-          <Button />
+          <Button 
+            title="Começar"
+          />
         </View>
 
       </View>
