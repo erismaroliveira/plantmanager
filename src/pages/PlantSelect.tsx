458d77e98b51ac40204b9197e78b06bd9fd45392
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-13 22:50:19 -0300

    Component navigation

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+import colors from '../styles/colors';
+
+export function PlantSelect() {
+  return (
+    <View style={styles.container}>
+      <Text>Selecionar Planta</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
