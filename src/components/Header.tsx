71b603031b70c8176b57106479e67342203433b4
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2021-05-13 22:51:46 -0300

    Component header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+} from 'react-native';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
