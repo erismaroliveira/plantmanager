415d66cf5176bd736a3dc40bb26158cf7942455f
erismar oliveira <erismarpro@hotmail.com>
2021-05-20 22:55:46 -0300

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,48 @@ import {
   View,
 } from 'react-native';
 
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import useImg from '../assets/erismar.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
 export function Header() {
   return (
     <View style={styles.container}>
-
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Erismar</Text>
+      </View>
+      <Image source={useImg} style={styles.image}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
   }
 });
