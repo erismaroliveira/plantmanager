a60adaab9fdc5c70086a382a6496c0678b5e6a58
erismar oliveira <erismarpro@hotmail.com>
2021-06-03 23:49:12 -0300

    save name user asyncstorage

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -12,6 +12,7 @@ import {
   Alert
 } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Button } from '../components/Button';
 
@@ -39,10 +40,12 @@ export function UserIdentification() {
     setName(value);
   }
 
-  function handleSubmit() {
+  async function handleSubmit() {
     if(!name)
       return Alert.alert('Me diz como chamar você 😢');
-      
+    
+    await AsyncStorage.setItem('@plantmanager:user', name);
+
     navigation.navigate('Confirmation');
   }
 
