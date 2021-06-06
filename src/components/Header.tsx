dbddf2b98b6a3b7241e50edb2ad9b838c369d366
erismar oliveira <erismarpro@hotmail.com>
2021-06-05 22:09:53 -0300

    Save and update username

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
   Image,
   View,
 } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import useImg from '../assets/erismar.png';
@@ -13,11 +13,24 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Header() {
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    async function loadStorageUserName() {
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+      setUserName(user || '');
+    }
+
+    loadStorageUserName();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.userName}>Erismar</Text>
+        <Text style={styles.userName}>
+          {userName}
+        </Text>
       </View>
       <Image source={useImg} style={styles.image}/>
     </View>
