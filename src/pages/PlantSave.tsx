3c6fa9f608ead157bccbf934db24a8f6860d1b26
Erismar Oliveira <erismarpro@hotmail.com>
2021-06-19 21:45:34 -0300

    Component plantsave

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   Alert,
   StyleSheet, 
@@ -10,28 +10,73 @@ import {
   TouchableOpacity
 } from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { useRoute } from '@react-navigation/core';
+import { format, isBefore } from 'date-fns';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 
 import { Button } from '../components/Button';
 
 import waterdrop from '../assets/waterdrop.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    }
+  }
+}
 
 export function PlantSave() {
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios');
+
+  const route = useRoute();
+  const { plant } = route.params as Params;
+
+  function handleChangeTime(event: Event, dateTime: Date | undefined){
+    if(Platform.OS === 'android'){
+      setShowDatePicker(oldState => !oldState);
+    }
+
+    if(dateTime && isBefore(dateTime, new Date())){
+      setSelectedDateTime(new Date());
+      return Alert.alert('Escolha um horário futuro! ⏰');
+    }
+
+    if(dateTime){
+      setSelectedDateTime(dateTime);
+    }
+  }
+
+  function handleOpenDateTimePickerForAndroid(){
+    setShowDatePicker(oldState => !oldState);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri
-          uri=""
+          uri={plant.photo}
           height={150}
           width={150}
         />
 
         <Text style={styles.plantName}>
-          Nome da Planta
+          {plant.name}
         </Text>
 
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate inventore ipsa tempora? Quod, numquam natus. Enim fugit necessitatibus voluptatem ullam sunt ut nemo, alias excepturi minima est doloribus quia vero!
+          {plant.about}
         </Text>
       </View>
 
@@ -43,13 +88,35 @@ export function PlantSave() {
           />
 
           <Text style={styles.tipText}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto impedit blanditiis nesciunt officia id excepturi. Repudiandae dolore autem atque adipisci laboriosam sint nulla molestias eum doloribus placeat! Nulla, vel non!
+            {plant.water_tips}
           </Text>
         </View>
 
         <Text style={styles.alertLabel}>
           Escolha o melhor horário para ser lembrado:
         </Text>
+
+        {showDatePicker && (
+          <DateTimePicker
+          value={selectedDateTime}
+          mode="time"
+          display="spinner"
+          onChange={handleChangeTime}
+          />
+        )}
+
+        {
+          Platform.OS === 'android' && (
+            <TouchableOpacity 
+              style={styles.dateTimePickerButton}
+              onPress={handleOpenDateTimePickerForAndroid}
+            >
+              <Text style={styles.dateTimePickerText}>
+                {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
+              </Text>
+            </TouchableOpacity>
+          )
+        }
 
         <Button
           title="Cadastrar planta"
@@ -74,8 +141,63 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: colors.shape
   },
+  controller: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20,
+  },
+  plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15,
+  },
   plantAbout: {
-
-  }
+    textAlign: 'center',
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+    marginTop: 10
+  },
+  tipContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: colors.blue_light,
+    padding: 20,
+    borderRadius: 20,
+    position: 'relative',
+    bottom: 60
+  },
+  tipImage: {
+    width: 56,
+    height: 56,
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 17,
+    textAlign: 'justify'
+  },
+  alertLabel: {
+    textAlign: 'center',
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+    marginBottom: 5
+  },
+  dateTimePickerButton: {
+    width: '100%',
+    alignItems: 'center',
+    paddingVertical: 40
+  },
+  dateTimePickerText: {
+    color: colors.heading,
+    fontSize: 24,
+    fontFamily: fonts.text
+  },
 });
 
