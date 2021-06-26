fc3cf9901c9f824af46627fcff1cb3b4c6514e14
Erismar Oliveira <erismarpro@hotmail.com>
2021-06-26 18:35:42 -0300

    Update userIdentification

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -46,7 +46,13 @@ export function UserIdentification() {
     
     try {
       await AsyncStorage.setItem('@plantmanager:user', name);
-      navigation.navigate('Confirmation');
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho',
+        subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantSelect'
+      });
     } catch {
       Alert.alert('Não foi possível salvar o nome do usuário. 😢');
     }  
